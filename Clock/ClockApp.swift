066c6216ff4c9d14f0d4a8daa16ef65765919d89
Thomas Chao <Tchao@lcs.on.ca>
2024-11-08 08:50:50 -0500

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Yuk Yeung Chao on 2024-11-08.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
